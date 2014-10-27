e1f95a972890726a29d05bd56669b019f2ab3bb6
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-10-28 00:44:16 +0400

    added inside pressure force

--- BouncingBall.py
+++ BouncingBall.py
@@ -12,9 +12,12 @@ class Ball():
         self._nodesColor  = nodesColor
         self._linesColor  = linesColor
         
+        self._pressure    = 80000.0
+        
         for i in xrange(0, nodeCount):
             angle = 2.0*math.pi * i / nodeCount
             N = Node(Point(self._center._x + radius * math.cos(angle), self._center._y + radius * math.sin(angle)), 10, 1)
+            N.addExternalForce(self.pump)
             self._nodes.append(N)
             
         for node, idx in zip(self._nodes, range(0, len(self._nodes))):
@@ -27,11 +30,31 @@ class Ball():
                 pygame.draw.line(surface, self._linesColor, node._pos.toTuple(), intNode._pos.toTuple())
             pygame.draw.circle(surface, self._nodesColor, node._pos.toTuple(), 2)
             
-    def pump(self):
-        return Point()
+    def update(self, dt):
+        self._center = Point()
+        for node in self._nodes:
+            node.update(dt)
+            self._center += node._pos
+        self._center._x /= len(self._nodes)
+        self._center._y /= len(self._nodes)
+        
+    def area(self):
+        sum = 0.0
+        for node, idx in zip(self._nodes, range(0, len(self._nodes))):
+            next_idx = (idx + 1) % len(self._nodes)
+            sum += (node._pos._x + self._nodes[next_idx]._pos._x)*(node._pos._y - self._nodes[next_idx]._pos._y)
+        return 0.5 * abs(sum)
+
+    def pump(self, node):
+        dP = self._pressure / self.area()
+        direction = self._center.normalize(node._pos)
+        return direction * dP
     
 def gravity(node):
     return Point(0, 9.8)
+
+def floor(node):
+    return Point(0.0, 0.0 if node._pos._y < 500.0 else -10 * node._pos._y)
 
 def main():
     WIDTH                = 800
@@ -45,6 +68,9 @@ def main():
     surface = pygame.Surface((WIDTH, HEIGHT))
     
     B = Ball(Point(200, 200), 100, (255, 0, 0), (0, 255, 0))
+    for node in B._nodes:
+        node.addExternalForce(gravity)
+        node.addExternalForce(floor)
     
     while True:
         for e in pygame.event.get():
@@ -52,7 +78,7 @@ def main():
                 raise SystemExit
             
         surface.fill(BG_COLOR)
-        
+        B.update(1.0 / 30.0)
         B.draw(surface)
             
         window.blit(surface, (0, 0))
